6f4efbfba0d27a066f9e7fcc2a6621fb9d598118
Enrico Lumetti <lumo3c273@gmail.com>
2012-04-30 14:13:26 +0200

    Google code jam

---
+++ googlecodejam/2009/1C/a.py
@@ -0,0 +1,20 @@
+def solve(string):
+	symbols = []
+	for digit in string:
+		if digit not in symbols:
+			symbols += digit
+			
+	base = max(2, len(symbols))
+	values = [1,0] + range(2, base)
+	
+	result = 0
+	for digit in string:
+		result *= base
+		result += values[symbols.index(digit)]
+		
+	return result
+
+T = int(input())
+for t in xrange(1, T+1):
+	result = solve(raw_input())
+	print "Case #%d: %d" %(t, result) 
