4d2992bd1b49bdbdfbbd682c3eea670827379d86
Enrico Lumetti <lumo3c273@gmail.com>
2012-08-01 11:59:41 +0200

    Modified sudoku test

--- exercises/sudoku/testmain.py
+++ exercises/sudoku/testmain.py
@@ -4,15 +4,15 @@ from solver import SudokuSolver
 
 sudoku = Sudoku(
     chain.from_iterable([
-        [7, 5, 6, 8, None, None, None, None, None],
-        [None, 9, 2, None, None, None, None, None, None],
-        [4, None, 3, None, 6, 7, None, 5, 2],
-        [None, 8, 9, 6, None, None, None, 3, None],
-        [None, None, None, 5, 9, 3, None, None, None],
-        [None, 7, None, None, None, 2, 9, 1, None],
-        [5, 2, None, 1, 3, None, 4, None, 9],
-        [None, None, None, None, None, None, 1, 2, None],
-        [None, None, None, None, None, 8, 7, 6, 5]
+        [   9, None, None, None, None, None,    6, None,   8],
+        [None, None,    7, None, None, None, None, None, None],
+        [None,    1, None,    8,    6,    2, None, None, None],
+        [None, None, None, None,    4, None,    5, None,    1],
+        [   4, None, None, None,    5, None, None, None,    6],
+        [   5, None,    9, None,    8, None, None, None, None],
+        [None, None, None,    6,    1,    7, None,    9, None],
+        [None, None, None, None, None, None,    4, None, None],
+        [   1, None,    8, None, None, None, None, None,    3]
     ]), 3)
 
 
