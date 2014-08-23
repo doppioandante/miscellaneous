8ae863673c585874f6183f1eb44f0e4c06ef8fc0
Enrico Lumetti <enrico.lumetti@gmail.com>
2014-08-23 18:51:03 +0200

    Starting again!

---
+++ projecteuler/problem1.hs
@@ -0,0 +1,8 @@
+sumMultiplesUpTo upto m = let n = (upto `div` m) in (`div` 2) $ m * n * (n+1)
+
+main = putStrLn $ show  result
+	where
+		result = 
+			let upto = 999
+			in sumMultiplesUpTo upto 3 + sumMultiplesUpTo upto 5 - sumMultiplesUpTo upto 15 
+			
