616300af0f7f530b5327da1ca93721fba7078a03
Enrico Lumetti <doppioandante@users.noreply.github.com>
2014-11-13 11:52:40 +0100

    Update Fractal.java
    Added some action listeners

--- fractalviewer/Fractal.java
+++ fractalviewer/Fractal.java
@@ -6,9 +6,14 @@ import javax.swing.JFrame;
 import java.awt.geom.Point2D;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 import java.text.NumberFormat;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+//TODO: change import granularity
 
-public class Fractal extends JFrame
+public class Fractal extends JFrame implements ActionListener, ChangeListener
 {
 	public static void main(String[] args) throws Exception
 	{
@@ -36,18 +41,20 @@ public class Fractal extends JFrame
 	public Fractal()
 	{
 		super();
+        
+        this.drawer = new HistogramMTMandelbrotDrawer(
+                new Point2D.Double(0.0, 0.0),
+				-2.0, 1.0, -1.0, 1.0,
+				1.0,
+				512,
+				Runtime.getRuntime().availableProcessors());
 		
 		createUI();
-		FractalPanel panel = new FractalPanel(
+		this.panel = new FractalPanel(
 				new Dimension(1024, 1024/3*2),
-				new HistogramMTMandelbrotDrawer(
-					new Point2D.Double(0.0, 0.0),
-					-2.0, 1.0, -1.0, 1.0,
-					1.0,
-					512,
-					Runtime.getRuntime().availableProcessors()));
+				this.drawer);
 
-		add(panel, BorderLayout.CENTER);
+		add(this.panel, BorderLayout.CENTER);
 	}
 
 	private void createUI()
@@ -57,8 +64,12 @@ public class Fractal extends JFrame
 		toolbar.setFloatable(false);
 
 		JButton saveButton = new JButton("Save");
-		//TODO: add action
+		saveButton.setActionCommand("save");
 		JButton resetButton = new JButton("Reset");
+        resetButton.setActionCommand("reset");
+
+		saveButton.addActionListener(this);
+        resetButton.addActionListener(this);
 
 		JLabel threadLabel = new JLabel("#Threads:");
 		int processors = Runtime.getRuntime().availableProcessors();
@@ -109,5 +120,31 @@ public class Fractal extends JFrame
 
 		setTitle("Fractal Viewer");
 	}
-}
 
+    public void actionPerformed(ActionEvent e)
+    {
+        if(e.getActionCommand().equals("reset"))
+        {
+            this.drawer.setOrigin(new Point2D.Double(0.0, 0.0));
+            this.drawer.setZoom(1.0);
+            this.panel.repaint();
+        }
+		else if(e.getActionCommand().equals("save"))
+		{
+			BufferedImage img = new BufferedImage(
+				this.panel.getWidth(),
+				this.panel.getHeight(),
+				BufferedImage.TYPE_INT_RGB);
+
+			this.drawer.draw(img.createGraphics(), this.panel.getSize());
+			// TODO: rename file if existing(and maybe move this code in another class)
+			try{
+			ImageIO.write(img, "jpg", new File("fractal.jpg"));
+			}catch(Exception ex) {}
+			JOptionPane.showMessageDialog(this, "Fractal written to fractal.jpg");
+		}
+    }
+
+    private AbstractMTFractalDrawer drawer;
+    private FractalPanel panel;
+}
