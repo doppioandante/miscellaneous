cda158ad570ecf0e9dafea30c3cdb88965dba3cd
Enrico Lumetti <enrico.lumetti@gmail.com>
2015-01-05 00:13:16 +0100

    Added reference impl of excercise 1, some corrections

--- upenn194/hw4/wholemeal.hs
+++ upenn194/hw4/wholemeal.hs
@@ -1,9 +1,21 @@
+fun1 :: [Integer] -> Integer
+fun1 [] = 1
+fun1 (x:xs)
+    | even x = (x - 2) * fun1 xs
+    | otherwise = fun1 xs
+
+fun2 :: Integer -> Integer
+fun2 1 = 0
+fun2 n
+    | even n = n + fun2 (n `div` 2)
+    | otherwise = fun2 (3*n + 1)
+
 fun1' :: [Integer] -> Integer
-fun1' = foldl (*) 0 . map (\x -> x - 2) . filter even
+fun1' = foldl (*) 1 . map (\x -> x - 2) . filter even
 
 fun2' :: Integer -> Integer
-fun2' = sum . takeWhile (\x -> x /= 1) . iterate collatz
+fun2' = sum . filter even . takeWhile (\x -> x /= 1) . iterate collatz
     where
-	collatz n 
+	collatz n
 	    | even n = n `div` 2
 	    | otherwise = 3*n + 1
