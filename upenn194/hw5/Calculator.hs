11d8595191fed54b63a4e3bc5c6b3f0b1fca1608
Enrico Lumetti <enrico.lumetti@gmail.com>
2015-01-05 11:20:47 +0100

    Almost finished hw5

--- upenn194/hw5/Calculator.hs
+++ upenn194/hw5/Calculator.hs
@@ -19,3 +19,37 @@ instance Expr ExprT where
     lit = Lit
     add = Add
     mul = Mul
+
+instance Expr Integer where
+    lit = id
+    add = (+)
+    mul = (*)
+
+instance Expr Bool where
+    lit = (>0)
+    add = (||)
+    mul = (&&)
+
+newtype MinMax = MinMax Integer deriving (Eq, Show)
+
+instance Expr MinMax where
+    lit = MinMax
+    add (MinMax a) (MinMax b) = MinMax $ max a b
+    mul (MinMax a) (MinMax b) = MinMax $ min a b
+
+newtype Mod7 = Mod7 Integer deriving (Eq, Show)
+
+instance Expr Mod7 where
+    lit x = Mod7 (mod7 x)
+    add (Mod7 a) (Mod7 b) = Mod7 $ mod7 $ a + b 
+    mul (Mod7 a) (Mod7 b) = Mod7 $ mod7 $ a * b
+    
+mod7 = (`mod` 7) 
+
+testExp :: Expr a => String -> Maybe a
+testExp = parseExp lit add mul
+
+testInteger = testExp :: String -> Maybe Integer
+testBool = testExp :: String -> Maybe Bool
+testMinMax = testExp :: String -> Maybe MinMax
+testMod7 = testExp :: String -> Maybe Mod7
